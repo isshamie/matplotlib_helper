dd584aebc0168ffdb6bedd504760a2cdcd1f538f
isshamie <isshamie@eng.ucsd.edu>
2022-02-24 14:45:03 -0500

    Update fig_utils

--- mplh/fig_utils.py
+++ mplh/fig_utils.py
@@ -22,7 +22,9 @@ import brewer2mpl
 sys.setrecursionlimit(3000)
 
 
-def helper_save(f_save=None, remove_bg=True, to_svg=True, dpi=300):
+def helper_save(f_save=None, remove_bg=True, to_svg=False, dpi=300,
+                facecolor='white', transparent=True, to_pdf=True, to_save=True, f=None,
+                is_grid=False):
     """
     Function to save figure file as png and svg if f_save is not None. Creates a
     transparent background.
@@ -32,19 +34,25 @@ def helper_save(f_save=None, remove_bg=True, to_svg=True, dpi=300):
         If True, remove the background of the plot
     :param to_svg : bool
         If true, save as an svg
+    :param f: figure to save. Uses current figure if None. default: None,
     :return:
     """
-    if remove_bg:
+    if f is None:
         f = plt.gcf()
+    elif is_grid: # seaborn grid object
+        f = f.figure
+    if remove_bg:
+        #f = plt.gcf()
         f.patch.set_facecolor("w")
         axs = f.get_axes()
         for ax in axs:
-            ax.set_facecolor('white')
+            if facecolor == 'white':
+                ax.set_facecolor('white')
             ax.spines['top'].set_visible(False)
             ax.spines['right'].set_visible(False)
             ax.spines['bottom'].set_visible(False)
             ax.spines['left'].set_visible(False)
-    if f_save is not None and not f_save == "":
+    if (f_save is not None and not f_save == "") and to_save:
         #Remove suffixes
         f_save = f_save.replace('.csv', '')
         f_save = f_save.replace('.tsv', '')
@@ -52,11 +60,14 @@ def helper_save(f_save=None, remove_bg=True, to_svg=True, dpi=300):
         f_save = f_save.replace(".png", "")
 
         # Saving
-        plt.savefig(f_save+".png", bbox_inches="tight", transparent=True,
+        f.savefig(f_save+".png", bbox_inches="tight", transparent=transparent,
                     dpi=dpi, pad_inches=0.1)
 
         if to_svg:
-            plt.savefig(f_save + '.svg')
+            f.savefig(f_save + '.svg')
+
+        if to_pdf:
+            f.savefig(f_save + '.pdf')
     else:
         print("No file to save in.")
     return
@@ -150,6 +161,7 @@ def stack_bars(x, ys, ylabels, f_save=None, use_text=True,deci=0):
 print("fig_utils from mplh")
 
 
+
 def df_stacked_bar(files, column="annotation", f_save=None,use_text=True):
     """ Creates a stacked barplot for each file, based on the column of choice."""
     anno_labels = set()
@@ -191,5 +203,82 @@ def show_values_on_bars(axs,deci=2):
     if isinstance(axs, np.ndarray):
         for idx, ax in np.ndenumerate(axs):
             _show_on_single_plot(ax)
-    else:
-        _show_on_single_plot(axs)
+    #else:
+
+
+from matplotlib.transforms import Bbox
+
+# def auto_fit_fontsize(text, width, height, fig=None, ax=None):
+#     '''Auto-decrease the fontsize of a text object.
+#
+#     Args:
+#         text (matplotlib.text.Text)
+#         width (float): allowed width in data coordinates
+#         height (float): allowed height in data coordinates
+#     '''
+#     fig = fig or plt.gcf()
+#     ax = ax or plt.gca()
+#
+#     # get text bounding box in figure coordinates
+#     renderer = fig.canvas.get_renderer()
+#     bbox_text = text.get_window_extent(renderer=renderer)
+#
+#     # transform bounding box to data coordinates
+#     bbox_text = Bbox(ax.transData.inverted().transform(bbox_text))
+#
+#     # evaluate fit and recursively decrease fontsize until text fits
+#     fits_width = bbox_text.width < width if width else True
+#     fits_height = bbox_text.height < height if height else True
+#     if not all((fits_width, fits_height)):
+#         text.set_fontsize(text.get_fontsize() - 1)
+#         auto_fit_fontsize(text, width, height, fig, ax)
+#
+#
+# def auto_fit_fontsize_suplabel(text, fig, axes, lab_type="ylabel"):
+#     renderer = fig.canvas.get_renderer()
+#     bbox_text = text.get_window_extent(renderer=renderer)
+#     print('bbox_text')
+#     print(bbox_text)
+#
+#     # Get left most axes bounding box
+#     left_most = 1
+#     for ax in axes:
+#         ax_bbox = Bbox(ax.transData.inverted())
+#         print('ax_bbox')
+#         print(ax_bbox)
+#         left_most = min(left_most, ax_bbox[0])
+#     return
+
+
+def facet_utils(g, dat, sup_ylabel=None, sup_title=None,
+                sup_xlabel=None,
+                title_add_n=True, group_col="ID", title_size=24,
+                ylabel_size=24, xlabel_size=24,
+                group_txt_d=None, group_txt=None,
+                subtitle_size='xx-large'):
+
+    for ax in g.axes.flat:
+        # Make x and y-axis labels slightly larger
+        # ax.get_yaxis().set_visible(False)
+        #     ax.set_xlabel(ax.get_xlabel(), fontsize='x-large')
+        ax.set_ylabel("")  # ax.get_ylabel(), fontsize='x-large')
+        # Make title more human-readable and larger
+        if ax.get_title():
+            curr_id = ax.get_title().split('=')[1].strip()
+            new_t = curr_id
+            if title_add_n:
+                new_t = f"{new_t}\nn={(dat[group_col] == new_t).sum()}"
+            if group_txt_d is not None:
+                new_t = f"{new_t}\n{group_txt}={group_txt_d[curr_id]}"
+            ax.set_title(new_t, fontsize=subtitle_size)
+    if sup_title is not None:
+        g.fig.subplots_adjust(top=0.8)
+        g.fig.suptitle(sup_title, size=title_size)
+    if sup_ylabel is not None:
+        txt = g.fig.text(0.001, 0.5, sup_ylabel, va='bottom', ha='center',
+                         rotation='vertical', size=ylabel_size, wrap=True)
+    if sup_xlabel is not None:
+        txt = g.fig.text(0.001, 0.5, sup_xlabel, va='bottom', ha='center',
+                         rotation='horizontal', size=xlabel_size, wrap=True)
+    plt.tight_layout() # Prevents axes overlapping each other
+    return g
